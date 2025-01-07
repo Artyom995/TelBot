1ce84275a77776f24a563a16b8cf9e80af336c85
artiom miasovskii <artyomka-5.5@mail.ru>
2025-01-08 02:01:46 +0500

    - api

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 # Файл для хранения конфигураций, включая токен
-API_TOKEN = '8091370425:AAFF5n20I53O-cFJ9gjPgOnpuEwCtHNakcQ'
+API_TOKEN = ''
 DB_NAME = 'quiz_bot.db'
 # user_answers = {}
 # Структура квиза
